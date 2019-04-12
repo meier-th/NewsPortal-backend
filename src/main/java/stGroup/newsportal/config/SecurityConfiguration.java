8c55602e0bd21f0294d32f448795e06d489eb85a
steinmann-th <steinmann.thms@gmail.com>
2019-04-12 18:20:07 +0300

    JDBC Authentication is added

--- src/main/java/stGroup/newsportal/config/SecurityConfiguration.java
+++ src/main/java/stGroup/newsportal/config/SecurityConfiguration.java
@@ -2,9 +2,12 @@ package stGroup.newsportal.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
@@ -24,7 +27,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                     .and()
                     .logout().deleteCookies("JSESSIONID").logoutSuccessUrl("/");
         } catch (Exception error) {
-            System.out.println("Security initialization error has occured:");
+            System.out.println("Security initialization error has occurred:");
+            System.out.println(error.getMessage());
+            error.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    @Autowired
+    public void initialize(AuthenticationManagerBuilder builder, DataSource dataSource) {
+        try {
+            builder.jdbcAuthentication().dataSource(dataSource);
+        } catch (Exception error) {
+            System.out.println("AuthenticationManager initialization has thrown an exception:");
             System.out.println(error.getMessage());
             error.printStackTrace();
             System.exit(1);
